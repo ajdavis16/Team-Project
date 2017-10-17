7a69905b0b16b68b6fb225027f6251404cf5bec2
Christian Ingram <clingram1@aol.com>
2017-10-17 09:31:58 -0400

    Added nav components and image

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import {Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { HeaderComponent } from './header/header.component';
+
+
+const appRoutes: Routes =[
+  { path: '', component: AppComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'signin', component: SigninComponent },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SigninComponent,
+    SignupComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
